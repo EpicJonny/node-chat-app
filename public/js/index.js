6cb3bdc28ba89ce72ea504916086c0038efa2268
Jonathan Mendonca <jonodmend@hotmail.com>
2019-03-03 18:36:09 +0000

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,12 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'John',
-        text: 'Hey!'
-    });
 });
 
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (msg) {
-    console.log('New message recieved', msg);
+socket.on('newMessage', function (message) {
+    console.log('New message recieved', message);
 });
